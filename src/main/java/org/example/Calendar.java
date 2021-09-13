5a491ec88fc14c2145a9a79880f6d604d4a67552
JeremyR <kidnip@knight.ucf.edu>
2021-09-12 22:15:29 -0400

    Initial commit

---
+++ src/main/java/org/example/Calendar.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Jeremy Rosales
+ */
+public class Calendar
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+        int n;
+        String month = null;
+
+        System.out.print("Please enter the number of the month: ");
+        n = input.nextInt();
+
+        switch(n){
+            case 1:
+                month = "January";
+                break;
+            case 2:
+                month = "February";
+                break;
+            case 3:
+                month = "March";
+                break;
+            case 4:
+                month = "April";
+                break;
+            case 5:
+                month = "May";
+                break;
+            case 6:
+                month = "June";
+                break;
+            case 7:
+                month = "July";
+                break;
+            case 8:
+                month = "August";
+                break;
+            case 9:
+                month = "September";
+                break;
+            case 10:
+                month = "October";
+                break;
+            case 11:
+                month = "November";
+                break;
+            case 12:
+                month = "December";
+                break;
+        }
+
+        System.out.println("The name of the month is " +month +".");
+    }
+}
